39814a36db4ccf696636bacc6a1bd41030061c66
amar <cheikh.amar@groupe-esigelec.org>
2025-01-02 03:41:05 +0100

    mis_a_jour_test_spyder

--- scrapy_app/test_spider.py
+++ scrapy_app/test_spider.py
@@ -1,6 +1,7 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from price_scraper.spiders.boulanger_spider import BoulangerSpider
+from price_scraper.spiders.cdiscount_spider import CdiscountSpider
 
 def test_spider():
     settings = get_project_settings()
@@ -9,6 +10,7 @@ def test_spider():
     settings.set('ITEM_PIPELINES', {'price_scraper.pipelines.MySQLPipeline': 300})
     
     process = CrawlerProcess(settings)
+    process.crawl(CdiscountSpider)
     process.crawl(BoulangerSpider)
     process.start()
 
