e16858007568b960ffdb88641cff28ed9674a3a7
aminatas24 <144959830+aminatas24@users.noreply.github.com>
2025-01-11 23:56:52 +0100

    Enhance EBaySpider 1

--- scrapy_app/price_scraper/spiders/ebay_spider.py
+++ scrapy_app/price_scraper/spiders/ebay_spider.py
@@ -23,3 +23,19 @@ class EBaySpider(scrapy.Spider):
         super().__init__(*args, **kwargs)
         # Configuration Chrome
         chrome_options = Options()
+
+                # Options pour éviter la détection
+        chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+        chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        chrome_options.add_experimental_option('useAutomationExtension', False)
+       
+        # Activation du mode headless
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-gpu")
+ 
+        # User agent et autres options
+        chrome_options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/120.0.0.0 Safari/537.36')
+        chrome_options.add_argument('--disable-notifications')
+        chrome_options.add_argument('--lang=fr-FR')
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
