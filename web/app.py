6f3c9c5421b4754b7765df62f53be40189bcc525
aminatas24 <144959830+aminatas24@users.noreply.github.com>
2025-01-14 13:00:47 +0100

    maj app.py 2

--- web/app.py
+++ web/app.py
@@ -15,3 +15,18 @@ def index():
                          products=data['products'],
                          current_page=data['current_page'],
                          total_pages=data['pages'])
+@app.route('/search')
+def search():
+    search_term = request.args.get('term', '')
+    if not search_term:
+        return jsonify({
+            'Boulanger': [],
+            'CDiscount': [],
+            'eBay': []
+        })
+   
+    products = db.search_products(search_term)
+    return jsonify(products)
+ 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
